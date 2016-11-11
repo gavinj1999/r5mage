b0eb07ece12c212edacc84cee399ccc184d69ffd
gavinj1999 <gavin.jones@rising5th.co.uk>
2016-11-11 16:38:34 +0000

    exec

--- git-hook.php
+++ git-hook.php
@@ -2,28 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 set_time_limit(0);
-
-try {
-
-  $payload = json_decode($_REQUEST['payload']);
-
+if(function_exists('exec')) {
+    echo "exec is enabled";
 }
-catch(Exception $e) {
-
-	//log the error
-	file_put_contents('/home/risingth/public_html/rising5th/mage/github.txt', $e . ' ' . $payload, FILE_APPEND);
-
-	  exit(0);
-}
-
-if ($payload->ref === 'refs/heads/master') {
-
-	$project_directory = '/home/risingth/public_html/rising5th/mage';
-
-	$output = shell_exec("/home/risingth/public_html/rising5th/mage/git-puller.sh");
-
-	//log the request
-	file_put_contents('/home/risingth/public_html/rising5th/mage/github.txt', $output, FILE_APPEND);
-
-}
+exec(dirname(__FILE__) . '/git-puller.sh');
 ?>
