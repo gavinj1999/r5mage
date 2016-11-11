ba6aeee58e7dd382cd4d9c1381e0940427c92f5a
gavinj1999 <gavin.jones@rising5th.co.uk>
2016-11-11 16:47:11 +0000

    dd

--- git-hook.php
+++ git-hook.php
@@ -11,16 +11,9 @@ exec(dirname(__FILE__) . '/git-puller.sh' );
 catch(Exception $e) {
 
 	//log the error
-	file_put_contents('/home/risingth/public_html/rising5th/mage/github.txt', $e . ' ' . $payload, FILE_APPEND);
+	file_put_contents('/home/risingth/public_html/rising5th/mage/github.txt', $e . ' ', FILE_APPEND);
 
 	  exit(0);
 }
 
-if ($payload->ref === 'refs/heads/master') {
-
-
-	//log the request
-	file_put_contents('/home/risingth/public_html/rising5th/mage/github.txt', $output, FILE_APPEND);
-
-}
 ?>
