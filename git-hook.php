61141532e11fdfd5a91c7a50d2c34fb92f9f955a
gavinj1999 <gavin.jones@rising5th.co.uk>
2016-11-11 16:45:46 +0000

    G

--- git-hook.php
+++ git-hook.php
@@ -2,8 +2,25 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 set_time_limit(0);
-if(function_exists('exec')) {
-    echo "exec is enabled";
-}
+
+try {
+
 exec(dirname(__FILE__) . '/git-puller.sh' );
+
+}
+catch(Exception $e) {
+
+	//log the error
+	file_put_contents('/home/risingth/public_html/rising5th/mage/github.txt', $e . ' ' . $payload, FILE_APPEND);
+
+	  exit(0);
+}
+
+if ($payload->ref === 'refs/heads/master') {
+
+
+	//log the request
+	file_put_contents('/home/risingth/public_html/rising5th/mage/github.txt', $output, FILE_APPEND);
+
+}
 ?>
