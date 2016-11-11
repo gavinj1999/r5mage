b0698587aec4f288b47ab2351f54a487ea371d28
gavinj1999 <gavin.jones@rising5th.co.uk>
2016-11-11 10:24:36 +0000

    add web hook

---
+++ git-hook.php
@@ -0,0 +1,29 @@
+<?php #!/usr/bin/env /usr/bin/php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+set_time_limit(0);
+
+try {
+
+  $payload = json_decode($_REQUEST['payload']);
+
+}
+catch(Exception $e) {
+
+	//log the error
+	file_put_contents('/srv/www/www.domain.com/logs/github.txt', $e . ' ' . $payload, FILE_APPEND);
+
+	  exit(0);
+}
+
+if ($payload->ref === 'refs/heads/master') {
+
+	$project_directory = '/srv/www/www.domain.com/public_html/';
+
+	$output = shell_exec("/srv/www/www.domain.com/public_html/git-puller.sh");
+
+	//log the request
+	file_put_contents('/srv/www/www.domain.com/logs/github.txt', $output, FILE_APPEND);
+
+}
+?>
